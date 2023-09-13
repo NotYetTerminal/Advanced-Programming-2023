302ee008d056171cf0640e94e97bcc0ba90f010b
Gabor Major <61381495+NotYetTerminal@users.noreply.github.com>
2023-09-13 10:41:31 +0100

    Lecture 1

---
+++ Lecture1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+    int firstVariable= 10;
+    char firstChar = 'a';
+
+    firstVariable = 100;
+    firstChar = 'b';
+
+    if (firstChar == 'b')
+    {
+
+    }
+    if (firstVariable == 23)
+    {
+
+    }
+
+
+    std::cout << "Hello, world!\n";
+
+    return 0;
+}
